45e85cd90b168180264f67e033a4b5b1ab4910ca
srhuang <lukyandy3162@gmail.com>
2019-03-22 00:53:45 +0800

    suuport multi-stack

--- Stack/stackUsingLinkedList.c
+++ Stack/stackUsingLinkedList.c
@@ -14,57 +14,93 @@ typedef struct Node{
     struct Node *next;
 }Node;
 
-Node *top=NULL;
-int isEmpty();
-void push(int); 
-int pop();
+typedef struct stack{
+    Node *top;
+}stack;
+
+stack* newStack();
+void deleteStack(stack *);
+int isEmpty(stack*);
+void push(int, stack*); 
+int pop(stack*);
+int peek(stack*);
 
 /*==============================================================*/
-int isEmpty(){
-    if(top==NULL){
+stack* newStack()
+{
+    stack *ret = malloc(sizeof(stack));
+    ret->top = NULL;
+
+    return ret;
+}
+
+void deleteStack(stack *myStack)
+{
+    while(myStack->top != NULL){
+        pop(myStack);
+    }
+    free(myStack);
+}
+
+int isEmpty(stack *myStack){
+    if(myStack->top==NULL){
         return 1; 
     }else{
         return 0;
     }
 }
 
-void push(int data){
+void push(int data, stack *myStack){
     Node *new = (Node*)malloc(sizeof(Node));
     new->data = data;
-    new->next = top;
-    top = new;
+    new->next = myStack->top;
+    myStack->top = new;
 }
 
-int pop(){
+int pop(stack *myStack){
 
-    if(isEmpty()){
+    if(isEmpty(myStack)){
         printf("Stack is empty\n");
         return -1;
     }else{
-        Node *temp = top;
+        Node *temp = myStack->top;
         int data = temp->data;
-        top = temp->next;
+        myStack->top = temp->next;
         free(temp);
         return data;
     }
 
 }
 
+int peek(stack *myStack)
+{
+    if(isEmpty(myStack)){
+        return -1;
+    }else{
+        return myStack->top->data;
+    }
+}
+
 /*==============================================================*/
 int main(){
+    stack *myStack = newStack();
     //push
     for(int i=0; i<10; i++){
-        push(i);
+        push(i, myStack);
     }
 
     //pop
     printf("Stack pop : ");
-    for(int i=0; i<10; i++){
-        int temp = pop();
+    for(int i=0; i<5; i++){
+        int temp = pop(myStack);
         printf("%d ", temp);
     }
     printf("\n");
-    
+
+    int temp = peek(myStack);
+    printf("%d ", temp);
+
+    deleteStack(myStack);
     return 0;
 }
 /*==============================================================*/
