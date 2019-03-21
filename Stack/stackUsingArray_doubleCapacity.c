85b025675786c7b28b4f1ceedd94ef3817be9a12
srhuang <lukyandy3162@gmail.com>
2019-03-22 00:36:26 +0800

    update stack to support multi-stack

--- Stack/stackUsingArray_doubleCapacity.c
+++ Stack/stackUsingArray_doubleCapacity.c
@@ -9,55 +9,92 @@ History :
 #include <stdlib.h>
 
 /*==============================================================*/
-int *stack;
-int top=-1;
-int capacity=3;
-int isEmpty();
-void push(int); 
-int pop();
+typedef struct stack{
+    int size;
+    int *container;
+    int top;
+}stack;
+
+stack* newStack(int);
+void doubleCapacity(stack *);
+void deleteStack(stack*);
+int isEmpty(stack*);
+void push(int, stack*); 
+int pop(stack*);
+int peek(stack*);
+int size(stack*);
 
 /*==============================================================*/
-int isEmpty(){
-    if(top==-1){
+stack* newStack(int size)
+{
+    if(size<=0)
+        return NULL;
+    stack *ret = malloc(sizeof(stack));
+    ret->size = size;
+    ret->container = malloc(sizeof(int) * size);
+    ret->top = -1;
+
+    return ret;
+}
+
+void deleteStack(stack *myStack){
+    free(myStack->container);
+    free(myStack);
+}
+
+int isEmpty(stack *myStack)
+{
+    if(myStack->top==-1){
         return 1; 
     }else{
         return 0;
     }
 }
 
-void doubleCapacity()
+void doubleCapacity(stack *myStack)
 {
-    capacity *= 2;
-    int *temp = stack;
-    stack = (int*)malloc(sizeof(int) * capacity);
-    printf("\nDouble the capacity : %d.\n", capacity);
-
-    //copy the data
-    for(int i=0; i<=top; i++){
-        stack[i] = temp[i];
-    }
-    free(temp);
+    myStack->size *= 2;
+    myStack->container = (int*)realloc(myStack->container, sizeof(int) * (myStack->size));
+    printf("\nDouble the capacity : %d.\n", myStack->size);
 }
 
-void push(int data){
-    if(top>capacity){
+int size(stack *myStack)
+{
+    return myStack->top+1;
+}
+
+void push(int data, stack *myStack){
+    if(myStack->top >= myStack->size){
         //printf("Stack is FULL\n"); 
-        doubleCapacity();
+        doubleCapacity(myStack);
 
     }
-    top++;
-    stack[top]=data;
-
+    myStack->top++;
+    myStack->container[myStack->top]=data;
+    
 }
 
-int pop(){
+int pop(stack *myStack){
     int data;
-    if(isEmpty()){
+    if(isEmpty(myStack)){
         printf("Stack is empty\n");
         return -1;
     }else{
-        data=stack[top];
-        top--;
+        data=myStack->container[myStack->top];
+        myStack->top--;
+        //printf("pop %d\n", data);
+        return data; 
+    }
+}
+
+int peek(stack *myStack)
+{
+    int data;
+    if(isEmpty(myStack)){
+        //printf("Stack is empty\n");
+        return -1;
+    }else{
+        data=myStack->container[myStack->top];
         return data; 
     }
 }
@@ -65,17 +102,17 @@ int pop(){
 /*==============================================================*/
 int main(){
     //Initialize
-    stack = (int*)malloc(sizeof(int) * capacity);
+    stack *myStack = newStack(10);
 
     //push
     for(int i=0; i<12; i++){
-        push(i);
+        push(i, myStack);
     }
 
     //pop
     printf("Stack pop : ");
     for(int i=0; i<12; i++){
-        int temp = pop();
+        int temp = pop(myStack);
         printf("%d ", temp);
     }
     printf("\n");
